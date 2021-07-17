1e630f37f771553f195e8329bf6b77ac682f959a
Candy Crush <76581343+Aadya-ai@users.noreply.github.com>
2021-07-17 09:39:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function first() {
+    window.location.href = "";
+}
+
+function second() {
+    window.location.href = "";
+}
+
+function third() {
+    window.location.href = "";
+}
+
+function fourth() {
+    window.location.href = "";
+}
+
+function Kchat() {
+    window.location.href = "";
+}
